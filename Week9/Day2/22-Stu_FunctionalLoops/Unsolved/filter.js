ca15baadb7723f356a679e5fd6c1243be0d3c338
KIL Bismuth <kiralowrey@gmail.com>
2020-08-23 20:47:45 -0700

    finished day 2

--- Week9/Day2/22-Stu_FunctionalLoops/Unsolved/filter.js
+++ Week9/Day2/22-Stu_FunctionalLoops/Unsolved/filter.js
@@ -1,6 +1,6 @@
 const originalArray = [1, 3, 2, 5, 10];
 
-const evenNumbers = originalArray.filter(function(data) {
+const evenNumbers = originalArray.filter(function (data) {
   if (data % 2 === 0) {
     return true;
   }
@@ -21,9 +21,12 @@ const isPrime = num => {
 // 1. A filter that runs through the original array and creates a new array containing only its prime numbers (`primeArray`)
 // Use the isPrime function provided to help with this
 
+const primeNumbers = originalArray.filter(isPrime);
+console.log(primeNumbers);
 
 // 2. A filter that runs through the original array and creates a new array containing only its numbers larger than 5(`moreThan5Array`)
 
-// Your code here
+const moreThan5Array = originalArray.filter((number) => number > 5);
+console.log(moreThan5Array);
 
 // Bonus: Use arrow functions as callbacks!
--- Week9/Day2/24-Stu_Template_Literals/Unsolved/index.js
+++ Week9/Day2/24-Stu_Template_Literals/Unsolved/index.js
@@ -1,13 +1,18 @@
 // music should be an object with title, artist, and album properties
 const music = {
-  // code here
+  title: "Grrrl Like",
+  artist: "Dope Saint Jude",
+  genre: "Hip-Hop",
+  album: "single",
+  year: "2018"
 };
 
 // Write code between the <div> tags to output the data from the music object above.
 // Use an h2 element for the title and a p element for artist and title
 const songSnippet = `
   <div class="song">
-
+    <h2>${music.title}</h2>
+    <p>${music.title} by ${music.artist} is a ${music.genre} song released as a ${music.album} in ${music.year}</p>
   </div>
 `;
 
