8b7df621dd004c1e1136e05a1012952009ce10ad
KIL Bismuth <kiralowrey@gmail.com>
2020-08-25 19:25:30 -0700

    installed npm

--- Week9/Day3/31-Stu_Callback-Review/Unsolved/index.js
+++ Week9/Day3/31-Stu_Callback-Review/Unsolved/index.js
@@ -12,3 +12,22 @@ fs.readFile("animals.json", "utf8", function(err, data) {
   const dogs = [];
   const cats = [];
 });
+
+// Instructor Code
+// const fs = require("fs");
+// // sync -- tell filesystem to read a file
+// fs.readFile("animals.json", "utf8", 
+// // async -- filesystem actually goes and reads the file
+// // VVVVV this is a callback
+// function(err, data) {
+//   // sync -- do this stuff once the file is read
+//   if (err) {
+//     // generic handle the error stuff.
+//     throw err;
+//   }
+//   // Parse the JSON string to an object
+//   const animalJSON = JSON.parse(data);
+//   // Create two new arrays to contain the cats and dogs objects
+//   const dogs = animalJSON.filter(animal => animal.species === 'dog');
+//   const cats = animalJSON.filter(animal => animal.species === 'cat');
+// });
